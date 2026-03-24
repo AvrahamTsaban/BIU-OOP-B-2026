7e745b6fb002757f49111e7864a28a6c7ec36e16
Avraham Tsaban <avraham.tsaban@gmail.com>
2026-03-24 17:14:46 +0200

    add zero-assignment files

--- ass0/HelloWorld.java
+++ ass0/HelloWorld.java
@@ -3,9 +3,16 @@
  */
 public class HelloWorld {
    /**
+<<<<<<< HEAD
     * Prints a short greeting five times.
     *
     * @param args command-line arguments (not used)
+=======
+    * This is a traditional demo class to demonstrate how to
+    *  write, build and run a Java program.
+    *
+    * @param args command line arguments.
+>>>>>>> ca59d8e (add zero-assignment files)
     */
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
