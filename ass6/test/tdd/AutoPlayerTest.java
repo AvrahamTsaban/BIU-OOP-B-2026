4cc95a4f15dd46f10262e1cf2fd21f9c44e8375c
Avraham Tsaban <avraham.tsaban@gmail.com>
2026-06-20 23:11:03 +0300

    final ass6

--- ass6/test/tdd/AutoPlayerTest.java
+++ ass6/test/tdd/AutoPlayerTest.java
@@ -2,6 +2,8 @@ package tdd;
 
 
 import java.util.Random;
+import java.util.Scanner;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,7 +15,7 @@ public class AutoPlayerTest {
     public void AutoPlayerMoveTest() {
         int size = rand.nextInt(7) + 3;
         Board board = new Board(size);
-        AutoPlayer autoPlayer = new AutoPlayer("Auto", 1, 'X');
+        AutoPlayer autoPlayer = new AutoPlayer("TestMe", 1, 'X');
         for (int i = 0; i < size * size;) {
             autoPlayer.move(board);
             boolean validMoveMade = countCells(board, 'X', ++i);
@@ -31,5 +33,39 @@ public class AutoPlayerTest {
             }
         }
         return count == expectedCount;
+    }
+
+    @Test
+    public void AutoPlayerGettersTest() {
+        AutoPlayer autoPlayer = new AutoPlayer("TestMe", 2, 'O');
+        Assert.assertEquals("TestMe", autoPlayer.getName());
+        Assert.assertEquals(2, autoPlayer.getId());
+        Assert.assertEquals('O', autoPlayer.getMarker());
+    }
+
+    @Test
+    public void AutoPlayerScannerTest() {
+        AutoPlayer autoPlayer = new AutoPlayer("TestMe", 3, 'X');
+        Scanner sc = autoPlayer.getSC();
+        Assert.assertNotNull(sc);
+        autoPlayer.closeSC();
+        try {
+            sc.nextLine();
+            Assert.fail("Expected an IllegalStateException to be thrown after closing the Scanner.");
+        } catch (IllegalStateException e) {
+            // Expected exception, test passes
+        }
+    }
+
+    @Test
+    public void AutoPlayerWinsTest() {
+        AutoPlayer autoPlayer = new AutoPlayer("TestMe", 44, 'O');
+        Assert.assertEquals(0, autoPlayer.getNumberOfWins());
+        for (int i = 0; i < 5; ++i) {
+            autoPlayer.incrementNumberOfWins();
+        }
+        Assert.assertEquals(5, autoPlayer.getNumberOfWins());
+        autoPlayer.resetNumberOfWins();
+        Assert.assertEquals(0, autoPlayer.getNumberOfWins());
     }
 }
