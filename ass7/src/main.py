ec826aa950599dfddc6848ff6c9f67a4c5a38f25
Avraham Tsaban <avraham.tsaban@gmail.com>
2026-06-30 12:34:27 +0300

    last ui tweaks

--- ass7/src/main.py
+++ ass7/src/main.py
@@ -1,20 +1,18 @@
 from board import Board
 from gui import GUI
 
-def get_number(prompt, min_value, max_value):
+def get_number(prompt):
     while True:
         try:
             value = int(input(prompt))
-            if min_value <= value <= max_value:
-                return value
-            else:
-                print(f"Please enter a number between {min_value} and {max_value}.")
+            return value
         except ValueError:
-            print("Invalid input. Please enter a valid integer.")
+            print("Please enter a positive integer.")
 
 def main():
-    grid_size = get_number("Enter the grid size (5-20): ", 5, 20)
-    mines_number = get_number(f"Enter the number of mines (1-{int(grid_size * grid_size / 3)}): ", 1, int(grid_size * grid_size / 3))
+    grid_size = get_number("""🎮  Welcome to Minesweeper!
+Enter the size of the board (e.g. 10 for 10×10): """)
+    mines_number = get_number(f"Enter the number of bombs: ")
     board = Board(grid_size, mines_number)
     gui = GUI(grid_size, board)
     gui.run()
